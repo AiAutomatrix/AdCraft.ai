37d58033fe45d1c981e057f4bfecc1fbcd75dc36
Garret Friesen <cogmoralabs@gmail.com>
2025-11-30 19:02:06 +0000

    Error: Cannot read properties of undefined (reading 'indexOf')
    src/hook

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-494843406-f3e2e",
   "appId": "1:186119598004:web:a1d56c245bba6fc3714ce1",
+  "storageBucket": "studio-494843406-f3e2e.appspot.com",
   "apiKey": "AIzaSyAoWSZfpkAP70MLG0LHW-J267bO8ODOpTk",
   "authDomain": "studio-494843406-f3e2e.firebaseapp.com",
   "measurementId": "",
